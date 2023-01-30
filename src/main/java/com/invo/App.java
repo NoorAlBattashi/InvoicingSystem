3f5dbcf675b8774a98adbba39f6dd07986b14422
NoorAlBattashi <nooralbattashi97@gmail.com>
2023-01-30 09:24:24 +0400

    create maven project

---
+++ src/main/java/com/invo/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.invo;
+
+/**
+ * @author LAP-9
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
